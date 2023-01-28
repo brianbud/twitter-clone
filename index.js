91fb37a50b233cb50bacf6fb8ac99d49c2b9801b
Brian Buddhadasa <brianbuddhadasa@gmail.com>
2023-01-28 00:48:10 -0500

    for of loop for feed html

--- index.js
+++ index.js
@@ -5,3 +5,33 @@ const tweetBtn = document.getElementById("tweet-btn");
 tweetBtn.addEventListener("click", function () {
   console.log(tweetInput.value);
 });
+
+function getFeedHtml() {
+  let feedHtml = ``;
+  for (let tweet of tweetsData) {
+    feedHtml = `
+<div class="tweet">
+    <div class="tweet-inner">
+        <img src="${tweet.profilePic}" class="profile-pic">
+        <div>
+            <p class="handle">${tweet.handle}</p>
+            <p class="tweet-text">${tweet.tweetText}</p>
+            <div class="tweet-details">
+                <span class="tweet-detail">
+                    ${tweet.replies.length}
+                </span>
+                <span class="tweet-detail">
+                    ${tweet.likes}
+                </span>
+                <span class="tweet-detail">
+                    ${tweet.retweets}
+                </span>
+            </div>   
+        </div>            
+    </div>
+</div>`;
+  }
+  return feedHtml;
+}
+
+getFeedHtml();
