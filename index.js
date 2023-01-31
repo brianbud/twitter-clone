5e991c7eb2f1ffde4a9bc2f9ebcc879b37f5a832
Brian Buddhadasa <brianbuddhadasa@gmail.com>
2023-01-30 22:24:06 -0500

    toggle show replies

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ document.addEventListener("click", function (e) {
     handleLikeClick(e.target.dataset.like);
   } else if (e.target.dataset.retweet) {
     handleRetweetClick(e.target.dataset.retweet);
+  } else if (e.target.dataset.reply) {
+    handleReplyClick(e.target.dataset.reply);
   }
 });
 
@@ -43,6 +45,10 @@ function handleRetweetClick(tweetId) {
 
   targetTweetObj.isRetweeted = !targetTweetObj.isRetweeted;
   render();
+}
+
+function handleReplyClick(replyId) {
+  document.getElementById(`replies-${replyId}`).classList.toggle("hidden");
 }
 
 function getFeedHtml() {
@@ -100,7 +106,7 @@ function getFeedHtml() {
             </div>   
         </div>            
     </div>
-    <div id="replies-${tweet.uuid}">
+    <div class= "hidden" id="replies-${tweet.uuid}">
     ${repliesHtml}
     </div>
 </div>
