a477724c5bccb6f66356d850e209b3d668969b1b
Brian Buddhadasa <brianbuddhadasa@gmail.com>
2023-01-30 23:21:11 -0500

    dont render if input has whitespace

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 import { tweetsData } from "./data.js";
 import { v4 as uuidv4 } from "https://jspm.dev/uuid";
 
-const tweetInput = document.getElementById("tweet-input");
-
 document.addEventListener("click", function (e) {
   if (e.target.dataset.like) {
     handleLikeClick(e.target.dataset.like);
@@ -51,19 +49,24 @@ function handleReplyClick(replyId) {
 }
 
 function handleTweetBtnClick() {
-  tweetsData.unshift({
-    handle: `@brianbud`,
-    profilePic: `images/user-avatar.png`,
-    likes: 0,
-    retweets: 0,
-    tweetText: tweetInput.value,
-    replies: [],
-    isLiked: false,
-    isRetweeted: false,
-    uuid: uuidv4(),
-  });
+  const tweetInput = document.getElementById("tweet-input");
 
-  render();
+  if (tweetInput.value.trim()) {
+    tweetsData.unshift({
+      handle: `@brianbud`,
+      profilePic: `images/user-avatar.png`,
+      likes: 0,
+      retweets: 0,
+      tweetText: tweetInput.value,
+      replies: [],
+      isLiked: false,
+      isRetweeted: false,
+      uuid: uuidv4(),
+    });
+
+    render();
+    tweetInput.value = "";
+  }
 }
 
 function getFeedHtml() {
